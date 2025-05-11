8ffc0a4cc0e7c7e32fa6e37e295cebba23c77e80
masanari0812 <sugomasa4649@gmail.com>
2025-05-11 14:31:02 +0900

    Revert "Ver1.11"
    This reverts commit 0670b3783b2ed98035dde64bfb2c890e3a6fb9cc.

--- src/linebot.js
+++ src/linebot.js
@@ -161,7 +161,7 @@ async function sendRegisterStatusMessage(userId, name, checkYear = undefined) {
     }
 }
 
-// 登録者チェック関数
+// 定期チェック関数
 async function periodicRegisterCheck() {
     console.log("定期的なチェック");
     await initDB();
@@ -192,16 +192,7 @@ app.post("/linebot", express.json(), async (req, res) => {
     res.sendStatus(200);
 });
 
-// Cronエンドポイント
-app.get("/cron", async (req, res) => {
-    try {
-        await periodicRegisterCheck();
-        res.sendStatus(200);
-    } catch (err) {
-        console.error("Cron error:", err);
-        res.sendStatus(500);
-    }
-});
+setInterval(periodicRegisterCheck, INTERVAL);
 
 // サーバ起動
 app.listen(PORT, () => {
